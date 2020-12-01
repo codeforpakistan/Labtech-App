13f4fe5863e86307b233a6e93a3f06493b6db215
Mubassir Hayat <mubassirhayat@gmail.com>
2020-12-01 16:07:12 +0500

    survey button added

--- lib/main.dart
+++ lib/main.dart
@@ -185,80 +185,177 @@ class MainPage extends StatelessWidget {
   }
 }
 
-enum SingingCharacter { yes, no }
+class SurveyPage extends StatefulWidget {
+  @override
+  _SurveyPageState createState() => _SurveyPageState();
+}
 
-class SurveyPage extends StatelessWidget {
-  SingingCharacter _character = SingingCharacter.yes;
+class _SurveyPageState extends State<SurveyPage> {
+  bool _checkedQ1 = false;
+  bool _checkedQ2 = false;
+  bool _checkedQ3 = false;
+  bool _checkedQ4 = false;
+  bool _checkedQ5 = false;
+  bool _checkedQ6 = false;
+  bool _checkedQ7 = false;
+  bool _checkedQ8 = false;
+  bool _checkedQ9 = false;
+  bool _checkedQ10 = false;
+  bool _checkedQ11 = false;
+  bool _checkedQ12 = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Survey Questions"),
-        ),
-        body: Container(
-          alignment: Alignment.topCenter,
-          child: Column(
-            children: <Widget>[
-              ListTile(
-                leading: Icon(Icons.check),
-                title: Text('Is the prompt and safe healthcare being assured?'),
-              ),
-              ListTile(
-                leading: Icon(Icons.check),
-                title: Text(
-                    'Are the patients being facilitated and properly guided at the triage+registration counter?'),
-              ),
-              ListTile(
-                leading: Icon(Icons.check),
-                title: Text(
-                    'Does the emergency environment support healthy interaction between patient and hospital staff?'),
-              ),
-              ListTile(
-                leading: Icon(Icons.check),
-                title: Text('Are the resources being properly utilized?'),
-              ),
-              ListTile(
-                leading: Icon(Icons.check),
-                title: Text(
-                    'Is the emergency having effective security & transport system?'),
-              ),
-              ListTile(
-                leading: Icon(Icons.check),
-                title: Text(
-                    'Does the emergency have adequate necessary resuscitation equipment & drugs?'),
-              ),
-              ListTile(
-                leading: Icon(Icons.check),
-                title: Text(
-                    'Does the emergency have adequate necessary human resources (Doctors, Nurses, Paramedical & support staff) available?'),
-              ),
-              ListTile(
-                leading: Icon(Icons.check),
-                title: Text(
-                    'Is the staff assigned to the duty in emergency present?'),
-              ),
-              ListTile(
-                leading: Icon(Icons.check),
-                title: Text(
-                    'Is the emergency clean with proper hospital waste management system?'),
-              ),
-              ListTile(
-                leading: Icon(Icons.check),
-                title: Text('Are the heating and cooling systems functional?'),
-              ),
-              ListTile(
-                leading: Icon(Icons.check),
-                title: Text('Is there adequate electrical and water supply?'),
-              ),
-              ListTile(
-                leading: Icon(Icons.check),
-                title: Text(
-                    'Are there adequate stretchers & wheelchairs available with adequate storage & maintenance capacity?'),
-              ),
-            ],
+    return StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+      return Scaffold(
+          appBar: AppBar(
+            title: Text("Survey Questions"),
           ),
-        ));
+          body: Center(
+            child: ListView(
+              shrinkWrap: true,
+              children: <Widget>[
+                CheckboxListTile(
+                  title:
+                      Text("Is the prompt and safe healthcare being assured?"),
+                  controlAffinity: ListTileControlAffinity.leading,
+                  value: _checkedQ1,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _checkedQ1 = value;
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title: Text(
+                      "Are the patients being facilitated and properly guided at the triage+registration counter?"),
+                  controlAffinity: ListTileControlAffinity.leading,
+                  value: _checkedQ2,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _checkedQ2 = value;
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title: Text(
+                      "Does the emergency environment support healthy interaction between patient and hospital staff?"),
+                  controlAffinity: ListTileControlAffinity.leading,
+                  value: _checkedQ3,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _checkedQ3 = value;
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title: Text("Are the resources being properly utilized?"),
+                  controlAffinity: ListTileControlAffinity.leading,
+                  value: _checkedQ4,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _checkedQ4 = value;
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title: Text(
+                      "Is the emergency having effective security & transport system?"),
+                  controlAffinity: ListTileControlAffinity.leading,
+                  value: _checkedQ5,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _checkedQ5 = value;
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title: Text(
+                      "Does the emergency have adequate necessary resuscitation equipment & drugs?"),
+                  controlAffinity: ListTileControlAffinity.leading,
+                  value: _checkedQ6,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _checkedQ6 = value;
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title: Text(
+                      "Does the emergency have adequate necessary human resources (Doctors, Nurses, Paramedical & support staff) available?"),
+                  controlAffinity: ListTileControlAffinity.leading,
+                  value: _checkedQ7,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _checkedQ7 = value;
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title: Text(
+                      "Is the staff assigned to the duty in emergency present?"),
+                  controlAffinity: ListTileControlAffinity.leading,
+                  value: _checkedQ8,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _checkedQ8 = value;
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title: Text(
+                      "Is the emergency clean with proper hospital waste management system?"),
+                  controlAffinity: ListTileControlAffinity.leading,
+                  value: _checkedQ9,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _checkedQ9 = value;
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title:
+                      Text("Are the heating and cooling systems functional?"),
+                  controlAffinity: ListTileControlAffinity.leading,
+                  value: _checkedQ10,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _checkedQ10 = value;
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title: Text("Is there adequate electrical and water supply?"),
+                  controlAffinity: ListTileControlAffinity.leading,
+                  value: _checkedQ11,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _checkedQ11 = value;
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title: Text(
+                      "Are there adequate stretchers & wheelchairs available with adequate storage & maintenance capacity?"),
+                  controlAffinity: ListTileControlAffinity.leading,
+                  value: _checkedQ12,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _checkedQ12 = value;
+                    });
+                  },
+                ),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context, true);
+                    },
+                    child: Text('Submit survey'),
+                  ),
+                )
+              ],
+            ),
+          ));
+    });
   }
-
-  void setState(Function() param0) {}
 }
