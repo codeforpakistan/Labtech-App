f8cfdf92b298f847435a7855616201818c17f762
Mubassir Hayat <mubassirhayat@gmail.com>
2020-12-01 14:43:46 +0500

    NIH survey flow

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'NIH Inspection App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'NIH Inspection App'),
     );
   }
 }
@@ -50,68 +51,214 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    final emailField = TextField(
+      obscureText: false,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Username",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+    final passwordField = TextField(
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+    final loginButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => MainPage()),
+          );
+        },
+        child: Text("Login",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
       ),
+    );
+
+    return Scaffold(
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 155.0,
+                  child: Image.asset(
+                    "assets/logo.png",
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                SizedBox(height: 45.0),
+                emailField,
+                SizedBox(height: 25.0),
+                passwordField,
+                SizedBox(
+                  height: 35.0,
+                ),
+                loginButton,
+                SizedBox(
+                  height: 15.0,
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hospital's List"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.medical_services),
+            title: Text('Polyclinic Hospital'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SurveyPage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.medical_services),
+            title: Text('PIMS Hospital Islamabad'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SurveyPage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.medical_services),
+            title: Text('CDA Hospital'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SurveyPage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.medical_services),
+            title: Text('Federal Government Dispensary - AGPR G-8/4'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SurveyPage()),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+enum SingingCharacter { yes, no }
+
+class SurveyPage extends StatelessWidget {
+  SingingCharacter _character = SingingCharacter.yes;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Survey Questions"),
+        ),
+        body: Container(
+          alignment: Alignment.topCenter,
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.check),
+                title: Text('Is the prompt and safe healthcare being assured?'),
+              ),
+              ListTile(
+                leading: Icon(Icons.check),
+                title: Text(
+                    'Are the patients being facilitated and properly guided at the triage+registration counter?'),
+              ),
+              ListTile(
+                leading: Icon(Icons.check),
+                title: Text(
+                    'Does the emergency environment support healthy interaction between patient and hospital staff?'),
+              ),
+              ListTile(
+                leading: Icon(Icons.check),
+                title: Text('Are the resources being properly utilized?'),
+              ),
+              ListTile(
+                leading: Icon(Icons.check),
+                title: Text(
+                    'Is the emergency having effective security & transport system?'),
+              ),
+              ListTile(
+                leading: Icon(Icons.check),
+                title: Text(
+                    'Does the emergency have adequate necessary resuscitation equipment & drugs?'),
+              ),
+              ListTile(
+                leading: Icon(Icons.check),
+                title: Text(
+                    'Does the emergency have adequate necessary human resources (Doctors, Nurses, Paramedical & support staff) available?'),
+              ),
+              ListTile(
+                leading: Icon(Icons.check),
+                title: Text(
+                    'Is the staff assigned to the duty in emergency present?'),
+              ),
+              ListTile(
+                leading: Icon(Icons.check),
+                title: Text(
+                    'Is the emergency clean with proper hospital waste management system?'),
+              ),
+              ListTile(
+                leading: Icon(Icons.check),
+                title: Text('Are the heating and cooling systems functional?'),
+              ),
+              ListTile(
+                leading: Icon(Icons.check),
+                title: Text('Is there adequate electrical and water supply?'),
+              ),
+              ListTile(
+                leading: Icon(Icons.check),
+                title: Text(
+                    'Are there adequate stretchers & wheelchairs available with adequate storage & maintenance capacity?'),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  void setState(Function() param0) {}
 }
